bf4ad56e82e2b01599009ac3289a5b17e054dd0e
kongaman <christian.kranz@gmail.com>
2019-03-01 11:11:24 +0100

    empty project

---
+++ android/app/src/main/java/de/ck/train/flutter_firebasecrud/MainActivity.java
@@ -0,0 +1,13 @@
+package de.ck.train.flutter_firebasecrud;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
